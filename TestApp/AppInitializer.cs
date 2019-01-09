17781600db45ded69caa769e3dfa53d2c718d3b2
CodeStyle <CodeStyle@protonmail.com>
2019-01-09 22:24:46 +0100

    com

---
+++ TestApp/AppInitializer.cs
@@ -0,0 +1,15 @@
+﻿using ObjCRuntime;
+using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TestApp
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Xamarin.UITest.Platform platform)
+        {           
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ TestAppXamarin/ViewController.cs
@@ -0,0 +1,49 @@
+﻿using Foundation;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.AppCenter.Analytics.iOS.Bindings;
+using ObjCRuntime;
+using UIKit;
+using Xamarin.Essentials;
+
+
+namespace TestAppXamarin
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        private ulong GetDiskSpace()
+        {
+            ulong totalSpace = 0;
+            NSError error = null;
+            string[] paths = NSSearchPath.GetDirectories(NSSearchPathDirectory.UserDirectory, NSSearchPathDomain.All);
+            var defManager = NSFileManager.DefaultManager;
+            var dicAttributes = defManager.
+                GetFileSystemAttributes(paths.First()
+                    , out error);
+            totalSpace = dicAttributes.Size;
+            return totalSpace;
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            fullabel.Text =
+                $"{Xamarin.Essentials.DeviceInfo.Model}{Environment.NewLine}{NSProcessInfo.ProcessInfo.PhysicalMemory}{Environment.NewLine}{GetDiskSpace()}";
+            MSAnalytics.TrackEvent($"{Xamarin.Essentials.DeviceInfo.Model}|{NSProcessInfo.ProcessInfo.PhysicalMemory}|{GetDiskSpace()}");
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
